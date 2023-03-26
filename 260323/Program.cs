469b519e9c0c172808738264a163eef2b5f2948e
Aleksei Piatachkov <pyatachkov.ao@gmail.com>
2023-03-26 13:10:24 +0500

    Added new work

---
+++ 260323/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая выводит случайное трехзначное
+ число и удаляет вторую цифру этого числа.
+● 456 -> 46
+● 782 -> 72
+● 918 -> 98
+*/
+
+
+Random random = new Random();
+int randomValue = random.Next(99, 1000);
+
+int firstDigits = randomValue / 100 * 10;
+
+int secomdDigits = randomValue % 10;
+
+Console.WriteLine(randomValue);
+Console.WriteLine(firstDigits + secomdDigits);
+
