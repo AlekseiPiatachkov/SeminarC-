bd5365b8d3df29720525a2cb4ccd8e0289d9e54a
Aleksei Piatachkov <pyatachkov.ao@gmail.com>
2023-04-04 17:36:58 +0500

    Added correction

--- 020423/Program.cs
+++ 020423/Program.cs
@@ -340,4 +340,45 @@ void Method21(int count, string text)
         i++;
     }
 }
+
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($" {i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
 */
+
+/* Дан текст. В тексте нужно все пробелы заменить вертикальной чёрточной, маленькие буквы "к" 
+заменить на большие буквы "К", а большие буквы "С" заменить на маленькие буквы "с".
+
+*/
+
+
+
+
+string text = " - Я думаю, - сказал князь, улыбаясь, что,"
+            + " ежели бы вас послали вместо нашего милого Винценгероде"
+            + " вы бы взяли приступом согласие прусского короля"
+            + " Вы так красноречивы, Вы дадите мне чаю";
+
+
+string ChengeText(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = ChengeText(text,' ', '|');
+Console.WriteLine(newText);
+newText = ChengeText(newText,'к', 'К');
+Console.WriteLine(newText);
